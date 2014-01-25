09f2f7377fcc626154be8a30f09f49b82a84c529
minimum2000 <minimum2000@mail.ru>
2014-01-25 05:27:09 +0400

    отредактированы бат-файлы, удалил скрины и текстовый документ

--- addressbook-selenium-tests/groups_xml.bat
+++ addressbook-selenium-tests/groups_xml.bat
@@ -1,3 +1,3 @@
 START 
 del groups.xml
-java -cp bin;D:\Курсы\5\xstream-1.4.6\lib\xstream-1.4.6.jar;D:\Курсы\5\xstream-1.4.6\lib\xstream\xpp3_min-1.1.4c.jar com.example.tests.GroupDataGenerator 10 groups.xml xml
+java -cp bin;D:\Курсы\5\xstream-1.4.6\lib\xstream-1.4.6.jar;D:\Курсы\5\xstream-1.4.6\lib\xstream\xpp3_min-1.1.4c.jar;D:\Курсы\5\xstream-1.4.6\lib\xstream\xmlpull-1.1.3.1.jar com.example.tests.GroupDataGenerator 10 groups.xml xml

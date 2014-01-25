021b4c9d3ea25a35781c13ee2795b6ee5897fe35
minimum2000 <minimum2000@mail.ru>
2014-01-25 05:58:16 +0400

    1 час 10 минут

---
+++ addressbook-selenium-tests/groups_testng.bat
@@ -0,0 +1,3 @@
+START 
+del groups.xml
+java -cp bin;D:\Курсы\5\xstream-1.4.6\lib\xstream-1.4.6.jar;D:\Курсы\5\xstream-1.4.6\lib\xstream\xpp3_min-1.1.4c.jar;D:\Курсы\5\xstream-1.4.6\lib\xstream\xmlpull-1.1.3.1.jar;D:\Курсы\Подготовка\selenium-2.38.0\selenium-server-standalone-2.38.0.jar org.testng.TestNG testng-customsuite.xml
